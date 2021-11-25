b285f2abab09c429a02e6e5c86f3cde6ccbb6fdc
Lenny McLennington <lennymclennington@protonmail.com>
2021-11-25 03:16:33 +0000

    penis

--- src/database/models/user.cc
+++ src/database/models/user.cc
@@ -1,8 +1,9 @@
 #include "base.hh"
 
-class User : Base {
+class User : public Base {
   public:
-	int cash;
+	double cash;
+	User(bsoncxx::document::value doc) : cash{doc.view()["cash"].get_double()} {}
 	bsoncxx::document::value to_document() {
 		bsoncxx::builder::stream::document builder{};
 		return builder << "cash" << cash << bsoncxx::builder::stream::finalize;
